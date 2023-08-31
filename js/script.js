385c70d7438c3300aa019a2bbf7301327a01b268
chester <techcode.edu@gmail.com>
2023-08-31 15:44:13 +0800

    solution

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// javascript programming
+
+// if (myHeading) {
+//     myHeading.textContent = "Hello world!";
+// } else {
+//     myHeading.textContent = "nothing is here"
+// }
+
+function sadaku() {
+    document.getElementById("col2action").innerHTML = "Bulaga!";
+}
